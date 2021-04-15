2b8615004c8c675a5dd961ecd1e20c32745a42c9
Cesar Escalona <cesarescalona10@gmail.com>
2021-04-15 12:40:20 -0400

    Added a PHP block containing a block comment header with info

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Author: Cesar Escalona
+ * Date: 04/15/2021
+ * URL: https://cescalona.greenriverdev.com/328/pp2/index.php
+ * Pair Program 2
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Program 2</title>
+</head>
+<body>
+  <h1>Pair Program 2</h1>
+</body>
+</html>
