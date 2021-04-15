6b7c3d6b71f0b04554a97f8082606294906a7ede
Cesar Escalona <cesarescalona10@gmail.com>
2021-04-15 19:19:43 -0400

    Defined an array of numbers

--- index.php
+++ index.php
@@ -19,5 +19,19 @@ error_reporting(E_ALL);
 </head>
 <body>
   <h1>Pair Program 2</h1>
+    <?php
+    $numbers = array("7", "9", "8", "9", "8", "8", "6");
+    function printArr($numbers)
+    {
+        foreach ($numbers as $number){
+            echo "$number <br>";
+        }
+    }
+    echo
+    "<h2> PHP Array Practice</h2>",
+    "<h3>2. Define an Array of numbers </h3>",
+    "<p>" . printArr($numbers) . "</p>";
+
+    ?>
 </body>
 </html>
